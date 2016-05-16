04f4c9bf1ba46c7e79fdedc1411cdcef0b0142e7
Lexiwu <sakura9641@gmail.com>
2016-05-16 21:17:47 +0800

    my third web 1.排版大致OK, 仍有問題要解決(row, validate) 2.特效還沒加

---
+++ js/form.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="jquery-1.12.3.js" />
+
+
+$(function () {
+
+    $(".send-btn").click(function () {
+        var validatot = $('.myform').validate({
+            rules: {
+                text_name:{required: true, maxlength:7 },
+                text_email: {required:true, email:true},
+                text_message:{required:true}
+            },
+            messages: {
+                text_name: { maxlength: "填寫正確名稱" },
+                text_email: { email: "error", required: "不得空白" },
+                text_message:{required:"Say Something"}
+            }
+
+        });
+
+
+
+
+
+    });
+
+
+
+
+
+
+
+});
