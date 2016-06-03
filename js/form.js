eb398d83c06985aa66d94605726b1f9f14331ff7
Jaja <a926a926@gmail.com>
2016-06-03 11:44:25 +0800

    adjust

--- js/form.js
+++ js/form.js
@@ -4,6 +4,7 @@
 $(function () {
 
     $(".send-btn").click(function () {
+
         var validatot = $('.myform').validate({
             rules: {
                 text_name:{required: true, maxlength:7 },
@@ -14,9 +15,22 @@ $(function () {
                 text_name: { maxlength: "填寫正確名稱" },
                 text_email: { email: "error", required: "不得空白" },
                 text_message:{required:"Say Something"}
+            },
+            errorPlacement: function (error, element) {
+                if (element.attr("name") == "text_name") {
+                    error.appendTo("#error_name");
+                }
+                if (element.attr("name") == "text_email") {
+                    error.appendTo("#error_email");
+                }
+                if (element.attr("name") == "text_message") {
+                    error.appendTo("#error_message");
+                }
             }
 
         });
+
+        $(".myform").validate();
 
 
 
